d132edd743698636a18a4ada693e613cfe29de5e
Khari Kisile <khari.kisile@gmail.com>
2019-11-02 13:33:16 -0500

    Updated Result/ResultView

--- src/Components/Result.jsx
+++ src/Components/Result.jsx
@@ -1,8 +1,16 @@
-import React, {useState, useEffect} from 'react';
+/* eslint-disable no-console */
+/* eslint-disable max-len */
+import React, { useState, useEffect } from 'react';
 import Flag from 'react-flags';
 import { Link } from 'react-router-dom';
-import { faStar } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import PropTypes from 'prop-types';
+import {
+  countryType,
+  userType,
+  dataType,
+} from '../Helpers/Types/index';
 import '../App.css';
 import { db } from './Firebase/firebase'
 // import * as ROUTES from '../Constants/Routes';
@@ -10,104 +18,144 @@ import { db } from './Firebase/firebase'
 
 const Result = props => {
   // const [show, setShow] = useState(false)
-  const [favorite, setFavorite] = useState(false)
+  const [favorite, setFavorite] = useState(false);
   // const [message, setMessage] = useState('');
 
-    useEffect(() => {
-      props.user && checkFavorite(props.country)
-    }, [])
+  const {
+    getCountryInfo,
+    filtered,
+    user,
+    country,
+    name,
+    subregion,
+    capital,
+    population,
+    flagCode,
+  } = props;
 
-    useEffect(() => {
-      props.user && checkFavorite(props.country)
-    }, [props.filtered]);
- 
-    const checkFavorite = (country) => {
-      const docRef = db.collection(`users/${props.user.uid}/favorites`).doc(`${country}`);
-      docRef.get()
-      .then(doc => {
-        if(doc){
-          if(doc.exists){
-            const data = doc.data()
-            setFavorite(true)
-            console.log(data)
+  console.log(props);
+
+  const checkFavorite = (coun) => {
+    const docRef = db.collection(`users/${user.uid}/favorites`).doc(`${coun}`);
+    docRef.get()
+      .then((doc) => {
+        if (doc) {
+          if (doc.exists) {
+            const data = doc.data();
+            setFavorite(true);
+            console.log(data);
           } else {
-            setFavorite(false)
+            setFavorite(false);
           }
         }
       }).catch(function(error) {
           console.log("Error getting document:", error);
       });
+  };
+  const makeFavorite = (e, coun) => {
+    e.persist();
+    // setShow(true)
+    if (!user) {
+      // setMessage({style: "warning", content: `You need to sign in to favorite countries. Login `, link: ROUTES.SIGN_IN, linkContent: 'here'})
+    } else if (!favorite) {
+      db.collection(`users/${user.uid}/favorites`).doc(`${coun.name}`).set({
+        coun,
+      }).then(() => {
+        console.log(`Added ${coun.name} to favorites`);
+        // setMessage({style: "success", content: `Added ${country.name} to favorites`})
+        setFavorite(true);
+      })
+        .catch((err) => {
+          console.error(err);
+          // setMessage({style: "danger", content: `Error adding ${country.name} to favorites, ${err}`})
+        });
+    } else {
+      db.collection(`users/${user.uid}/favorites`).doc(`${coun.name}`).delete()
+        .then(() => {
+          console.log(`Removed ${coun.name} from favorites`);
+          // setMessage({style: "warning", content: `Removed ${country.name} from favorites`})
+          setFavorite(false);
+          // setShow(true)
+        })
+        .catch((err) => {
+          console.error(err);
+          // setMessage({style: "danger", content: `Error adding ${country.name} to favorites, ${err}`})
+        });
     }
-    const makeFavorite = (e, country) => {
-      e.persist();
-      // setShow(true)
-      if(!props.user){
-        // setMessage({style: "warning", content: `You need to sign in to favorite countries. Login `, link: ROUTES.SIGN_IN, linkContent: 'here'})
-      } else {
-        if(!favorite){
-          db.collection(`users/${props.user.uid}/favorites`).doc(`${country.name}`).set({
-                country
-          }).then(() => {
-            console.log(`Added ${country.name} to favorites`)
-            // setMessage({style: "success", content: `Added ${country.name} to favorites`})
-            setFavorite(true)
-          }).catch((err) => {
-            console.error(err)
-            // setMessage({style: "danger", content: `Error adding ${country.name} to favorites, ${err}`})
-          })
-        } else {
-          db.collection(`users/${props.user.uid}/favorites`).doc(`${country.name}`).delete()
-          .then(() => {
-            console.log(`Removed ${country.name} from favorites`)
-            // setMessage({style: "warning", content: `Removed ${country.name} from favorites`})
-            setFavorite(false)
-            // setShow(true)
-          }).catch((err) => {
-            console.error(err)
-            // setMessage({style: "danger", content: `Error adding ${country.name} to favorites, ${err}`})
-          })
-        }
-      }
+  };
+
+  useEffect(() => {
+    if (user) {
+      checkFavorite(country);
     }
-        return(
-            <div className="mr-md-3 card mb-3">
-                <div className="result media">
-                    <div className="media-body">
-                    <h4 className="title">
-                        {props.name} ({props.flagCode})
-                        <br/><small>Capital: {props.capital && props.capital.split(';')[0]} | Pop: {props.population}</small> 
-                    </h4>
-                    <p className="subregion">
-                    <strong>Location: </strong>{props.subregion}
-                    </p>
-                    <Link to={`${process.env.PUBLIC_URL}/${props.name}`} className="btn btn-success btn-sm" onClick={() => props.getCountryInfo(props.name, props.capital)}>Read More</Link>
-                    </div>
-                    {props.user && <div className="stars"><FontAwesomeIcon onClick={(e) => makeFavorite(e, props.country)} size="2x" value={props.country} color={favorite ? "gold" : "gray"} icon={faStar} /></div>}
-                    <Flag 
-                        className="img-thumbnail"
-                        name={(props.flagCode)? props.flagCode : "_unknown"}
-                        format="svg"
-                        pngSize={64}
-                        shiny={false}
-                        alt={`${props.name}'s Flag`}
-                        basePath="/img/flags"
-                    />  
-                    
-                </div>
-                
-            </div>
+  }, []);
+  useEffect(() => {
+    if (user) {
+      checkFavorite(country);
+    }
+  }, [filtered]);
+  return (
+    <div className="mr-md-3 card mb-3">
+      <div className="result media">
+        <div className="media-body">
+          <h4 className="title">
+            {name}
+            {' '}
+            (
+            {flagCode}
+            )
+            <br />
+            <small>
+            Capital:
+              {' '}
+              {capital && capital.split(';')[0]}
+              {' '}
+              | Pop:
+              {' '}
+              {population}
+            </small>
+          </h4>
+          <p className="subregion">
+            <strong>Location: </strong>
+            {subregion}
+          </p>
+          <Link to={`${process.env.PUBLIC_URL}/${name}`} className="btn btn-success btn-sm" onClick={() => getCountryInfo(name, capital)}>Read More</Link>
+        </div>
+        {user && <div className="stars"><FontAwesomeIcon onClick={(e) => makeFavorite(e, country)} size="2x" value={country} color={favorite ? 'gold' : 'gray'} icon={faStar} /></div>}
+        <Flag
+          className="img-thumbnail"
+          name={(flagCode) || '_unknown'}
+          format="svg"
+          pngSize={64}
+          shiny={false}
+          alt={`${name}'s Flag`}
+          basePath="/img/flags"
+        />
+      </div>
+    </div>
 
-            // <div className="card h-100 col-sm-12 col-md-9 mb-3">
-            //     <img className="card-img" src={props.flag} alt={props.imgalt}/>
-            //     <div className="card-img-overlay">
-            //         <h5 className="card-title">{props.name} <small>Capital: {props.capital}</small></h5>
-            //         <p>Pop: {props.population}</p>
-            //         <p className="card-text"><strong>Region: </strong>{props.region}</p>
-            //         <p className="subregion"><strong>Subregion: </strong>{props.subregion}</p>
-            //         <button className="btn btn-primary btn-sm">Read More</button>
-            //     </div>
-            // </div>
-        )
-}
+  // <div className="card h-100 col-sm-12 col-md-9 mb-3">
+  //     <img className="card-img" src={flag} alt={imgalt}/>
+  //     <div className="card-img-overlay">
+  //         <h5 className="card-title">{name} <small>Capital: {capital}</small></h5>
+  //         <p>Pop: {population}</p>
+  //         <p className="card-text"><strong>Region: </strong>{region}</p>
+  //         <p className="subregion"><strong>Subregion: </strong>{subregion}</p>
+  //         <button className="btn btn-primary btn-sm">Read More</button>
+  //     </div>
+  // </div>
+  );
+};
 
+Result.propTypes = {
+  getCountryInfo: PropTypes.func.isRequired,
+  filtered: dataType.isRequired,
+  user: userType.isRequired,
+  country: countryType.isRequired,
+  name: PropTypes.string.isRequired,
+  subregion: PropTypes.string.isRequired,
+  capital: PropTypes.string.isRequired,
+  population: PropTypes.number.isRequired,
+  flagCode: PropTypes.string.isRequired,
+};
 export default Result;
