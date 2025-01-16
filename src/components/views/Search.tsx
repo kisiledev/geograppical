0a9b68fd6d10888861bad0a893ea150216fa7ed3
kisiledev <70709254+kisiledev@users.noreply.github.com>
2025-01-16 14:34:59 -0600

    Update Search.tsx

--- src/components/views/Search.tsx
+++ src/components/views/Search.tsx
@@ -4,17 +4,27 @@ import React from 'react';
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import '../../App.css';
 
-const Search = props => {
-
-    return(
-      <div>
-        <div className="input-group mb-3">
-            <input type="text" className="form-control" placeholder="Type a country to begin" aria-label="" aria-describedby="basic-addon1" value={props.searchText} onChange={this.props.passInput}/>
-            <div className="input-group-append">
-            </div>
-          </div>
-        </div>  
-    );
+interface SearchProps {
+  searchText: string;
+  passInput: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
+const Search = ({ searchText, passInput }: SearchProps) => {
+  return (
+    <div>
+      <div className="input-group mb-3">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Type a country to begin"
+          aria-label=""
+          aria-describedby="basic-addon1"
+          value={searchText}
+          onChange={passInput}
+        />
+        <div className="input-group-append"></div>
+      </div>
+    </div>
+  );
+};
 
 export default Search;
